a90e1752222bcc0e4ed9d71b3d23f3354b3af601
junyounga <cjy874545@gmail.com>
2020-02-24 03:02:15 +0900

    Create AppToolbar.vue

---
+++ components/AppToolbar.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-toolbar color="primary" fixed dense>
+    <v-toolbar-title class="ml-0 pl-3">
+      <v-app-bar-nav-icon @click.stop="toggleDrawer()"></v-app-bar-nav-icon>
+    </v-toolbar-title>
+  </v-toolbar>
+</template>
+
+<script>
+export default {
+  name: 'app-toolbar',
+  data: function() {
+    return {}
+  },
+  methods: {
+    toggleDrawer() {
+      alert('22')
+    }
+  }
+}
+</script>
