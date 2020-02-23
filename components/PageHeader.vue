5c2aabd61644fd93cb5be6c347dc7de6729595ce
junyounga <cjy874545@gmail.com>
2020-02-24 03:02:18 +0900

    Create PageHeader.vue

---
+++ components/PageHeader.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-layout row class="align-center layout px-4 pt-4 app--page-header">
+    <div class="page-header-left">
+      <h3 class="pr-3">{{title}}</h3>
+    </div>
+    <v-icon>mdi-home</v-icon>
+
+  </v-layout>
+</template>
+<script>
+export default {
+  name: 'PageHeader',
+  data() {
+    return {
+      title: ''
+    }
+  }
+}
+</script>
