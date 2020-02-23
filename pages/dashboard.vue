892c1055edcaeed108277029a41373d636f6c069
junyounga <cjy874545@gmail.com>
2020-02-23 23:29:35 +0900

    dashboard add

---
+++ pages/dashboard.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="pageDashboard">
+        <v-container grid-listxl fluid>
+            <v-layout row wrap>
+                    <v-flex lg3 sm6 xs12>
+                        <mini-statistic
+                            icon="fa fa-facebook"
+                            title="100+"
+                            sub-title="Likes"
+                            color="indigo"
+                        >
+                        </mini-statistic>
+                    </v-flex>
+            </v-layout>
+        </v-container>
+    </div>
+</template>
+
+<script>
+import MiniStatistic from '@/components/widgets/MiniStatistic.vue'
+export default {
+  components: {
+    MiniStatistic
+  }
+}
+</script>
