e1f5612a517a169d53ea82a847ab1f974df7b739
junyounga <cjy874545@gmail.com>
2020-02-24 16:58:44 +0900

    Update AppToolbar.vue

--- components/AppToolbar.vue
+++ components/AppToolbar.vue
@@ -1,9 +1,29 @@
 <template>
-  <v-toolbar color="primary" fixed dense>
+  <v-app-bar  color="primary darken-2" fixed dense app>
     <v-toolbar-title class="ml-0 pl-3">
       <v-app-bar-nav-icon @click.stop="toggleDrawer()"></v-app-bar-nav-icon>
     </v-toolbar-title>
-  </v-toolbar>
+    <v-spacer></v-spacer>
+    <v-row align="center" style="max-width: 650px;">
+      <v-text-field
+        flat
+        solo-inverted
+        prepend-icon="mdi-magnify"
+        label="Search"
+        class="hidden-sm-and-down"
+        hide-details
+      ></v-text-field>
+    </v-row>
+    <v-spacer></v-spacer>
+    <v-row>
+      <v-btn icon>
+        <v-icon>fa-2x fa-github</v-icon>
+      </v-btn>
+      <v-btn icon @click="handleFullScreen()">
+        <v-icon>mdi-fullscreen</v-icon>
+      </v-btn>
+    </v-row>
+  </v-app-bar>
 </template>
 
 <script>
@@ -15,6 +35,9 @@ export default {
   methods: {
     toggleDrawer() {
       alert('22')
+    },
+    handleFullScreen() {
+      alert('full screen')
     }
   }
 }
