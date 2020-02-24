deacb8bd853816d64f245ac4c093ea0b1d7e41be
junyounga <cjy874545@gmail.com>
2020-02-24 19:24:13 +0900

    Update AppDrawer.vue

--- components/AppDrawer.vue
+++ components/AppDrawer.vue
@@ -5,8 +5,9 @@
     app
     :dark="$vuetify.dark"
     width="260"
+    fixed
   >
-    <v-toolbar color="primary darken-3" dense>
+    <v-toolbar color="primary darken-3" dense fixed>
       <v-toolbar-title class="ml-0 pl3">
         <span class="hidden-sm-and-down">Vue.js X Nuxt.js</span>
       </v-toolbar-title>
@@ -14,28 +15,32 @@
     <perfect-scrollbar>
       <v-list dense expand>
         <template v-for="item in menus">
+          <!-- group width subitems -->
           <v-list-group
             v-if="item.items"
             :key="item.name"
             :group="item.group"
             :prepend-icon="item.icon"
-            no-action="no-action"
+            no-action
           >
             <template v-slot:activator>
               <v-list-item-content>
                 <v-list-item-title>{{item.title}}</v-list-item-title>
               </v-list-item-content>
             </template>
-
-            <template v-for="subItem in item.items">
+            <!-- sub group-->
+            <template v-for="(subItem, i) in item.items">
               <v-list-group
                 v-if="subItem.items"
                 :key="subItem.name"
-                :group="subItem.group"
-                sub-group="sub-group"
-              >
-                <v-list-tile slot="activator" ripple="ripple"></v-list-tile>
-              </v-list-group>
+                sub-group
+                :group="subitem.group"
+              ></v-list-group>
+              <v-list-item v-else :key="i" :to="subItem.href ? subItem.href : null">
+                <v-list-item-contnet>
+                  <v-list-item-title>{{subItem.title}}</v-list-item-title>
+                </v-list-item-contnet>
+              </v-list-item>
             </template>
           </v-list-group>
         </template>
