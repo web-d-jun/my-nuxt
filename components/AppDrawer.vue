2d1674bee34a190b138b417adb1354d7ed0d461b
junyounga <cjy874545@gmail.com>
2020-02-24 18:40:31 +0900

    Update AppDrawer.vue

--- components/AppDrawer.vue
+++ components/AppDrawer.vue
@@ -12,19 +12,46 @@
       </v-toolbar-title>
     </v-toolbar>
     <perfect-scrollbar>
-        <v-list dense expand>
-            
+      <v-list dense expand>
+        <template v-for="item in menus">
+          <v-list-group
+            v-if="item.items"
+            :key="item.name"
+            :group="item.group"
+            :prepend-icon="item.icon"
+            no-action="no-action"
+          >
+            <template v-slot:activator>
+              <v-list-item-content>
+                <v-list-item-title>{{item.title}}</v-list-item-title>
+              </v-list-item-content>
+            </template>
 
-        </v-list>
+            <template v-for="subItem in item.items">
+              <v-list-group
+                v-if="subItem.items"
+                :key="subItem.name"
+                :group="subItem.group"
+                sub-group="sub-group"
+              >
+                <v-list-tile slot="activator" ripple="ripple"></v-list-tile>
+              </v-list-group>
+            </template>
+          </v-list-group>
+        </template>
+      </v-list>
     </perfect-scrollbar>
   </v-navigation-drawer>
 </template>
 <script>
+import menu from '@/api/menu'
+
 export default {
   name: 'app-drawer',
   data: () => ({
     mini: false,
-    
-  })
+    menus: menu
+  }),
+  created() {}
 }
 </script>
