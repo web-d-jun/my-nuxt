c074d181ab3a3bbae95701ff03348a3828c51cd5
junyounga <cjy874545@gmail.com>
2020-03-18 10:36:33 +0900

    Create Calculator.vue

---
+++ components/widgets/Calculator.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="Calculator">
+      Calculator
+  </div>
+</template>
+<script lang="ts">
+import { Vue, Component } from 'nuxt-property-decorator'
+
+export default class Calculator extends Vue {
+
+}
+</script>
+<style lang="scss" scoped>
+</style>
